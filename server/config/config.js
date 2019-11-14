beb8683b169d6432cf60b03f4ea5af5c0411d697
Steven Rivas <srivasgtc@gmail.com>
2019-11-14 10:38:36 -0500

    Se implemntó la devolución del token a la hora de realizar el login

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,13 @@
 process.env.PORT = process.env.PORT || 3000;
 
 //========================================
+// Authorization y Authentication
+//========================================
+
+process.env.CADUCIDAD_TOKEN = 60 * 5
+process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
+
+//========================================
 // ENTORNO
 //========================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
@@ -14,9 +21,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 bd_usuario = 'steven-rivas';
 bd_password = 'jN91wfLypysZhV5T';
 
-if(process.env.NODE_ENV==='dev'){
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
-}else{
+} else {
     urlDB = `mongodb+srv://${bd_usuario}:${bd_password}@cluster0-wtu9g.mongodb.net/cafe?retryWrites=true&w=majority`
 }
 
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,14 +1,45 @@
 const express = require('express')
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Usuario = require("../models/usuario");
+const _ = require("underscore");
 const app = express();
 
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {
 
-    res.json({
-        ok:true,
-        message:"Bueno"
-    })
+
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioBD) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                message: err
+            });
+        }
+
+        if (!usuarioBD || !bcrypt.compareSync(body.password, usuarioBD.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "El usuario o contraseña son incorrectos."
+                }
+            });
+        }
+
+        usuarioBD = _.pick(usuarioBD, ['nombre', 'email', 'img', 'role', 'estado']);
+
+        let token = jwt.sign({
+            usuario: usuarioBD
+        }, "este-es-el-seed-desarrollo", { expiresIn: process.env.CADUCIDAD_TOKEN})
+
+        res.json({
+            ok: true,
+            usuario: usuarioBD,
+            token
+        })
+
+    });
 });
 
 
